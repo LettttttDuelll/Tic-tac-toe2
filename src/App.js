8cd678a3cf57d1029c51bb5b2aa6754789f0b735
LettttttDuelll <phamdung07102004@gmail.com>
2025-07-21 14:19:00 +0700

    update to 5x5

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(25).fill(null));
 
   function handedClick(i) {
     if (squares[i] || calculateWinner(squares)) { return; }; // Ignore click if square is already filled
@@ -34,41 +34,57 @@ export default function Board() {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const nemBer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+
   return (
     <>
       <div className="status">{status}</div>
       <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={() => handedClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handedClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handedClick(2)} />
+        {nemBer.filter(num => num < 5).map((num) => (
+          <Square key={num} value={squares[num]} onSquareClick={() => handedClick(num)} />
+        ))}
       </div>
       <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={() => handedClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handedClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handedClick(5)} />
+        {nemBer.filter(num => num > 4 && num < 10).map((num) => (
+          <Square key={num} value={squares[num]} onSquareClick={() => handedClick(num)} />
+        ))}
       </div>
       <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={() => handedClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handedClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handedClick(8)} />
+        {nemBer.filter(num => num > 9 && num < 15).map((num) => (
+          <Square key={num} value={squares[num]} onSquareClick={() => handedClick(num)} />
+        ))}
+      </div>
+      <div className='board-row'>
+        {nemBer.filter(num => num > 14 && num < 20).map((num) => (
+          <Square key={num} value={squares[num]} onSquareClick={() => handedClick(num)} />
+        ))}
+      </div>
+      <div className='board-row'>
+        {nemBer.filter(num => num > 19 && num < 25).map((num) => (
+          <Square key={num} value={squares[num]} onSquareClick={() => handedClick(num)} />
+        ))}
       </div>
     </>
   );
 }
 function calculateWinner(squares) {
   const line = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+    [0, 6, 12, 18, 24],
+    [4, 8, 12, 16, 20]
   ];
-  for (let i = 0; i < line.length; i++) {
-    const [a, b, c] = line[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  for (const element of line) {
+    const [a, b, c,d,e] = element;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]&&squares[a]===squares[d] && squares[a]===squares[e]) {
       return squares[a]; // Return the winner ("X" or "O")
     }
   }
